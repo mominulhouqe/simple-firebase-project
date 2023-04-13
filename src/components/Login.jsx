f41436119757300c39d583c035b06a329677d242
mominul <ulhouqemomin39@gmail.com>
2023-04-13 15:22:31 +0600

    first commit

---
+++ src/components/Login.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
+import app from "../firebase/firebase.init";
+
+const Login = () => {
+  const [user, setUser] = useState(null);
+  const auth = getAuth(app);
+  const provider = new GoogleAuthProvider();
+
+  const handleGoogleLogin = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const logingUser = result.user;
+        console.log(logingUser);
+        setUser(logingUser);
+      })
+      .catch((error) => {
+        console.log("It's error broo", error.massage);
+      });
+  };
+const handleSingOut = ()=>{
+    signOut(auth)
+    .then(result=>{
+        console.log(result);
+        setUser(result)
+    })
+}
+  return (
+    <div>
+      <button onClick={handleGoogleLogin}> Google Login</button>
+      <button onClick={handleSingOut}>Sing Out</button>
+      {user && (
+        <div>
+          <h4>user: {user.displayName}</h4>
+          <p>Email: {user.email}</p>
+          <img src={user.photoURL} alt="" />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCNXy0PXxhzKXmv-GR-FNCUfbkZAEzdQQE",
+  authDomain: "simple-firbase-2acd8.firebaseapp.com",
+  projectId: "simple-firbase-2acd8",
+  storageBucket: "simple-firbase-2acd8.appspot.com",
+  messagingSenderId: "965748861817",
+  appId: "1:965748861817:web:0904067020be94d8a9dda8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
