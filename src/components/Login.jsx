836405f9a8125d826def41eecf34b0ea5518de59
mominul <ulhouqemomin39@gmail.com>
2023-04-14 10:33:49 +0600

    added Github

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,14 +6,17 @@ import {
   signOut,
 } from "firebase/auth";
 import app from "../firebase/firebase.init";
+import { GithubAuthProvider } from "firebase/auth";
 
 const Login = () => {
   const [user, setUser] = useState(null);
   const auth = getAuth(app);
-  const provider = new GoogleAuthProvider();
+  const googleProvider = new GoogleAuthProvider();
+  const githubProvider = new GithubAuthProvider();
 
   const handleGoogleLogin = () => {
-    signInWithPopup(auth, provider)
+
+    signInWithPopup(auth, googleProvider)
       .then((result) => {
         const logingUser = result.user;
         console.log(logingUser);
@@ -22,24 +25,49 @@ const Login = () => {
       .catch((error) => {
         console.log("It's error broo", error.massage);
       });
+
   };
+  const handleGithubSingIn = ()=>{
+    signInWithPopup(auth, githubProvider)
+    .then(result =>{
+      const loginUser = result.user;
+      setUser(loginUser);
+    })
+    .catch(error=>{
+      console.log("It' s Your ", error.massage);
+    })
+  }
+
+
   const handleSingOut = () => {
     signOut(auth).then((result) => {
       console.log(result);
       setUser(result);
     });
   };
+
   return (
     <div>
-      {user ? (
+      {
+      user ? 
         <button onClick={handleSingOut}>Sing Out</button>
-      ) : (
+       :
+      <>
+         
         <button onClick={handleGoogleLogin}> Google Login</button>
-      )}
+        <button onClick={handleGithubSingIn}>Github login</button>
+      
+
+      </>
+
+
+      }
+      
       {user && (
         <div>
           <h4>user: {user.displayName}</h4>
-          <p>Email: {user.email}</p>
+          <p>Email: {user.email }</p>
+          <p>LastSignIn Time: {user.metadata.lastSignInTime}</p>
           <img src={user.photoURL} alt="" />
         </div>
       )}
